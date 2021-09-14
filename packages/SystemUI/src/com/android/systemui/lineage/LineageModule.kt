12bb7239d59a0e302595329a991effd77b0e19df
jhonboy121 <alfredmathew05@gmail.com>
2021-09-14 20:23:04 +0530

    [SQUASH] SystemUI: introduce refresh rate tile
    * supports cycling between 60 to peak refresh rate and auto mode
    
    Co-authored-by: Michael Bestas <mkbestas@lineageos.org>
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Gustavo Mendes <gusttavo.me@outlook.com>

--- packages/SystemUI/src/com/android/systemui/lineage/LineageModule.kt
+++ packages/SystemUI/src/com/android/systemui/lineage/LineageModule.kt
@@ -31,6 +31,7 @@ import com.android.systemui.qs.tiles.UsbTetherTile
 import com.android.systemui.qs.tiles.VpnTile
 import com.android.systemui.qs.tiles.WifiTile
 import com.android.systemui.qs.tiles.DataSwitchTile
+import com.android.systemui.qs.tiles.RefreshRateTile;
 
 import dagger.Binds
 import dagger.Module
@@ -116,4 +117,10 @@ interface LineageModule {
     @IntoMap
     @StringKey(DataSwitchTile.TILE_SPEC)
     fun bindDataSwitchTile(dataSwitchTile: DataSwitchTile): QSTileImpl<*>
+
+    /** Inject refreshRateTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(RefreshRateTile.TILE_SPEC)
+    fun bindRefreshRateTile(refreshRateTile: RefreshRateTile): QSTileImpl<*>
 }
